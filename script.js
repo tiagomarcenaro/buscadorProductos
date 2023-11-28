b873505b1063f8c5090a4b7798e97cba1cb8e735
gastonmartinezz <ezegaston2011@gmail.com>
2023-11-28 18:16:41 -0300

    Proyecto finalizado
    Se termino la entrega correctamente.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let productos = [];
 // Arreglo para almacenar productos en el carrito
 let cart = [];
 
+let tabla = document.getElementById("cartTableBody");
+
 // Función para cargar productos desde el JSON
 async function loadProductsFromJSON() {
   try {
@@ -27,27 +29,65 @@ function loadProducts() {
   const tableBody = document.getElementById("productTableBody");
   tableBody.innerHTML = "";
   productos.forEach(product => {
-    const row = `<tr>
+    const row = 
+    `
+    <tr>
       <td>${product.id}</td>
       <td>${product.nombre}</td>
       <td>${product.descripción}</td>
       <td>${product.cantidad}</td>
       <td>${product.costo}</td>
-      <td><button onclick="addToCart(${product.id})">Agregar al Carrito</button></td>
-    </tr>`;
+      <td><button onclick="addToCart('${product.id}')">Agregar al Carrito</button></td>
+    </tr>s
+    `;
     tableBody.innerHTML += row;
   });
 }
 
+
+// Función para actualizar la tabla del carrito
+function updateCart() {
+  let totalPrice = 0;
+  tabla.innerHTML = '';
+
+  cart.forEach(item => {
+    const row = document.createElement("tr");
+    row.setAttribute("id", `cartItem_${item.id}`)
+    row.innerHTML = `
+      <td>${item.name}</td>
+      <td>${item.quantity}</td>
+      <td>${item.cost}</td>
+      <td>${item.quantity * item.cost}</td>
+      <td><button onclick="removeFromCart('${item.id}')">Eliminar</button></td>
+    `;
+    totalPrice += item.quantity * item.cost;
+    tabla.appendChild(row);
+  });
+  
+  document.getElementById("totalPrice").textContent = totalPrice;
+}
+
 function addToCart(productId) {
-  const product = productos.find(p => p.id === productId);
+  let product;
+  let b = false;
+
+  for (let i = 0; i < productos.length; i++) {
+    if (productId == productos[i].id) {
+      product = productos[i];
+      break;
+    }
+  }
 
   if (product && product.cantidad > 0) {
-    const cartItem = cart.find(item => item.id === productId);
+    for (let j = 0; j < cart.length; j++) {
+      if (product.id == cart[j].id) {
+        cart[j].quantity++;
+        b = true;
+        break;
+      }
+    }
 
-    if (cartItem) {
-      cartItem.quantity++;
-    } else {
+    if (b == false) {
       cart.push({
         id: product.id,
         name: product.nombre,
@@ -59,42 +99,40 @@ function addToCart(productId) {
     product.cantidad--;
     loadProducts();
     updateCart();
+
   } else {
     alert("No hay stock disponible para este producto.");
   }
 }
 
-
-
-
-
-
-// Función para actualizar la tabla del carrito
-function updateCart() {
-  cartTable.innerHTML = "";
-  let totalPrice = 0;
-  cart.forEach(item => {
-    const row = document.createElement("tr");
-    row.innerHTML = `
-      <td>${item.name}</td>
-      <td>${item.quantity}</td>
-      <td>${item.cost}</td>
-      <td>${item.quantity * item.cost}</td>
-      <td><button onclick="removeFromCart(${item.id})">Eliminar</button></td>
-    `;
-    cartTable.appendChild(row);
-    totalPrice += item.quantity * item.cost;
-  });
-  document.getElementById("totalPrice").textContent = totalPrice;
-}
-
+//Funcion para remover un articulo del carrito.
 function removeFromCart(productId) {
-  const product = productos.find(p => p.id === productId);
-  const cartItem = cart.find(item => item.id === productId);
-  
-  if (product && cartItem) {
+  let product;
+  for (let j = 0; j < productos.length; j++) {
+    if (productId == productos[j].id) {
+      product = productos[j];
+      break;
+    }
+  }
+
+  let cartItem;
+  let cartIndex;
+  for (let i = 0; i < cart.length; i++) {
+    if (productId == cart[i].id) {
+      cartItem = cart[i];
+      cartIndex = i;
+      break;
+    }
+  }
+
+  if (cartItem) {
     product.cantidad += cartItem.quantity;
-    cart = cart.filter(item => item.id !== productId);
+    
+    const cartTableRow = document.getElementById(`cartItem_${productId}`);
+    if (cartTableRow) {
+      cartTableRow.remove();
+    }
+    cart.splice(cartIndex, 1);
     updateCart();
   } else {
     console.error("Error al eliminar del carrito: Producto no encontrado en el carrito.");
